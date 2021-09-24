533649bd12c8e3082288a1a03d14ac3eabc3e75a
ovsharam <sharmaapoorva124@gmail.com>
2021-09-23 20:05:19 -0700

    Element Variables JS

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+//Initialize racer
+
+//------------------------------------------Show and Hide Track-----------------------------------------------------//
+// for(let i = 0; i < 10; i++){
+
+
+
+//Element Variables
+const targetDiv = document.getElementById("Track");
+const btn = document.getElementById("Phlash");
+const redDot = document.getElementById("Red_dot");
+const blueDot = document.getElementById("Blue_dot");
