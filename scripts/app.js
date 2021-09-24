af97cb9e909abe9daa3799788f1983a0b288ea06
ovsharam <sharmaapoorva124@gmail.com>
2021-09-23 20:06:46 -0700

    Hidden Elements

--- scripts/app.js
+++ scripts/app.js
@@ -21,3 +21,16 @@ btn.onclick = function() {
         targetDiv.style.display = 'none'
     }
 }
+
+const secondDiv = document.getElementById("Track");
+const secondBtn = document.getElementById("Soniq");
+secondBtn.onclick = function() {
+    if (secondDiv.style.display == 'none'){
+        secondDiv.style.display = 'block'
+        gameBtn.style.display = 'block'
+        // redDot.style.display = 'block'
+        // blueDot.style.display = 'block'
+    }else {
+        secondDiv.style.display = 'block'
+    }
+}
